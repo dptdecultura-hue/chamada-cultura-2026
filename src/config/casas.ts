431112fc95b163f61055e8ebc1a8934e62d134cc
dptdecultura-hue <dptdecultura@gmail.com>
2026-06-18 23:00:54 +0000

    versão trimestral com 3 unidades

---
+++ src/config/casas.ts
@@ -0,0 +1,6 @@
+// src/config/casas.ts
+export const configuracaoCasas: Record<string, { nome: string }> = {
+  "sede": { nome: "CASA DA CULTURA - SEDE" },
+  "centro": { nome: "CASA DA CULTURA - CENTRO" },
+  "jardim-europa": { nome: "CASA DA CULTURA - JARDIM EUROPA" }
+};
